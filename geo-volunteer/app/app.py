06e3338ad1dfc71b0a540d2657c85e9b4371eddf
linibensonjr <linibensonjr@gmail.com>
2022-11-13 11:28:13 +0100

    style: Update style for layout.html

--- geo-volunteer/app/app.py
+++ geo-volunteer/app/app.py
@@ -1,23 +1,100 @@
 from email import message
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+import os
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'geo-volunteer.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
+migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+
+class Manager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+
+class Organization(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(100), nullable=False)
+    zipcode = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=False)
+    website = db.Column(db.String(100), nullable=True)
+    description = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"Organization('{self.name}', '{self.email}')"
+
+class Opportunity(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.String(100), nullable=False)
+    time = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(100), nullable=False)
+    zipcode = db.Column(db.String(100), nullable=False)
+    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Event('{self.name}', '{self.description}')"
+
+class Volunteer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(100), nullable=False)
+    zipcode = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=False)
+    website = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"Volunteer('{self.name}', '{self.email}')"
+
+        
+
+db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Volunteer.query.get(int(user_id))
+
+
 
 @app.route('/')
 def home():
-    # message = {
-    #     'greeting': 'Hello World'
-    # }
-    # return (message)
-    return render_template('pages/index.html')
+    opportunities = Opportunity.query.all()
+
+    return render_template('pages/index.html', opportunities=opportunities)
 
 @app.route('/org')
 def org():
-    # message = {
-    #     'greeting': 'Hello World'
-    # }
-    # return (message)
-    return render_template('pages/index.html')
+    organizations = Organization.query.all()
+    return render_template('pages/index.html', organizations=organizations)
 
 if __name__ == '__main__':
     app.run(debug=True, port=81)
